ea7737fdd84d1d9707f3c8ae09b3697a330713a1
Ksojo98 <117241209+Ksojo98@users.noreply.github.com>
2026-02-06 16:42:51 -0700

    db context

--- APW.Data/ProductDbContext.cs
+++ APW.Data/ProductDbContext.cs
@@ -39,7 +39,7 @@ public partial class ProductDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        // aca no me puteen, mi sql tiene otro nombre :( esto se pudo haber evitado con docker 
+        // aca no me puteen, mi sql tiene otro nombre :( esto se pudo haber evitado con docker lo siento jiji 
         //=> optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ProductDB2;Trusted_Connection=True;TrustServerCertificate=True");
         => optionsBuilder.UseSqlServer("Server=Moke\\SQLEXPRESS01;Database=ProductDB;Trusted_Connection=True;TrustServerCertificate=True");
 
